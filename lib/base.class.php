012db5e261ca4401dfc2901cfb1a033b79345b4a
Jack.Wu <xiaowu365@gmail.com>
2013-06-03 10:21:07 +0800

    添加注释

--- lib/base.class.php
+++ lib/base.class.php
@@ -28,7 +28,7 @@ class Base
 	 * 
 	 * @param string $fileName			文件名称
 	 * @param string $fileType			文件类型 
-	 * @param int $type					导入目录类型:0->core, 1->plugins	
+	 * @param int $type			导入目录类型:0->core, 1->plugins	
 	 * @static
 	 * @access public
 	 * @return bool						返回导入标记
